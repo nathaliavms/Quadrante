7d5e7dd5842f7c7709c30baedfe5205b3dc22ed2
Nathalia Vieira <Silva.Nathalia@ibm.com>
2022-08-23 16:14:16 -0300

    first commit

---
+++ Quadrante/ImprimirQuadrante.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quadrante
+{
+    class ImprimirQuadrante
+    {
+        public int Coordenada1 { get; private set; }
+        public int Coordenada2 { get; private set; }
+
+        public ImprimirQuadrante(int coordenada1, int coordenada2)
+        {
+            Coordenada1 = coordenada1;
+            Coordenada2 = coordenada2;
+        }
+        public string EncontrarQuadrante()
+        {
+           if (Coordenada1 == 0 || Coordenada2 == 0)
+           {
+                return " ";
+           }
+           else if (Coordenada1 >= 1 && Coordenada2 >= 1)
+           {
+                return "primeiro";
+           }
+           else if (Coordenada1 < 0 && Coordenada2 >= 1)
+           {
+                return "segundo";
+           }
+           else if (Coordenada1 < 0 && Coordenada2 < 0)
+           {
+                return "terceiro";
+           }
+           else 
+           {
+                return "quarto";
+           }
+        }
+    }
+}
---
+++ Quadrante/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Quadrante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite duas cordenadas na mesma linha: !");
+            string[] coordenada = Console.ReadLine().Split(" ");
+            var coordenada1 = int.Parse(coordenada[0]);
+            var coordenada2 = int.Parse(coordenada[1]);
+           
+            ImprimirQuadrante resultado = new ImprimirQuadrante(coordenada1, coordenada2);
+            Console.WriteLine(resultado.EncontrarQuadrante()); 
+        }
+    }
+}
